70a30190274ec1f9b76b1d902a0ff4d4fc0db46e
TomSRemy <tom.s.remy@gmail.com>
2026-06-01 14:03:54 +0200

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -177,6 +177,7 @@ function showPage(id) {
   if (id === 'load' && !window._loadLoaded) loadLoad();
   if (id === 'overview') loadOverview();
   if (id === 'converter') { updateConverter(); updateCapacity(); }
+  if (id === 'analysis' && typeof loadMarketAnalysis === 'function') loadMarketAnalysis();
 }
 
 function switchSection(sec) {
